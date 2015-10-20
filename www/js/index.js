987ab9d7c5a53d6a0ae2a310049688cccc208780
JadMorgan <jmorgan.ad@googlemail.com>
2015-10-20 13:39:49 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,16 @@
 var key = "phone";
 var value = "xperia";
+var key2 = "pen";
+var value2 = "black";
 
 window.localStorage.setItem( key, value );
 
 
-var key2 = "phone";
-var value2 = window.localStorage.getItem(key);
+var key = "phone";
+var value = window.localStorage.getItem(key);
+
+var key2 = "pen";
+var value2 = window.localStorage.getItem(key2);
 
 alert(value);
 alert(value2);
