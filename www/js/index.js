29652e5bf1febf1decbafc2085c0cadd7ceb200b
JadMorgan <jmorgan.ad@googlemail.com>
2015-10-20 13:26:39 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,41 +1,11 @@
-var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
+var key = "phone";
+var value = "xperia";
 
-    function onLoad() {
-        document.addEventListener("deviceready", onDeviceReady, false);
-		console.log("device ready");
-    }
-	
-	function updateDisplay() {
-		$("#launched").text("Application launched: " + launched_count);
-		$("#resumed").text("Application paused: " + paused_count);
-		$("#paused").text("Application resumed: " + resumed_count);
-	}
+window.localStorage.setItem( key, value );
 
 
-    // device APIs are available
-    //
-    function onDeviceReady() {
-		alert("device ready");
-        
-		document.addEventListener("resume", onResume, false);
-		document.addEventListener("pause", onPause, false);
-		
-		launched_count++;
-		updateDisplay();
-    }
+var key2 = "phone";
+var value2 = window.localStorage.getItem(key);
 
-    // Handle the pause event
-    //
-    function onPause() {
-		alert("pause");
-		paused_count++;
-		updateDisplay();
-    }
-	
-	function onResume() {
-		alert("resume");
-		resumed_count++;
-		updateDisplay();
-    }
+alert(value);
+alert(value2);
